6482beeab126dc81e980960467258f65d51adc57
sohaibh55 <sohaibhamdi55@outlook.com>
2025-01-17 00:33:35 -0400

    we add two functions : insert and display

--- Graph.cpp
+++ Graph.cpp
@@ -5,14 +5,22 @@
 #include "Graph.h"
 
 
-Graph::Graph() {
+Graph::Graph(int size) : graph(size,vector<int>(size,0)) {}
 
-}
-
-void Graph::insert(int value)
+void Graph::insert(int u,int v,int distance,bool allow)
 {
-
-
-
-
+    assert( u < graph.size() && u >= 0 && v < graph.size() && v >= 0 && "Error : Invalid Index");
+   graph[u][v] = distance;
+   if (allow)
+     graph[v][u] = distance;
+}
+void Graph::display() {
+    for (int i = 0 ; i < graph.size() ; i++) {
+        cout<<i<<" : ";
+        for (int j = 0 ; j < graph.size() ; j++) {
+            if (graph[i][j] != 0)
+                cout<<" ( " <<j<<","<< graph[i][j] << " ) ";
+        }
+        cout<<endl;
+    }
 }
