4dbf37234d80caa226a3a741993315257f06efe0
sohaibh55 <sohaibhamdi55@outlook.com>
2025-01-16 23:08:35 -0400

    The Header File declares the functions

---
+++ Graph.h
@@ -0,0 +1,26 @@
+//
+// Created by sohai on 1/16/2025.
+//
+
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <iostream>
+#include <vector>
+using namespace std;
+class Graph
+{
+  private:
+     vector<vector<int>> graph;
+public:
+    Graph();
+    void insert(int value);
+
+
+};
+
+
+
+
+
+#endif //GRAPH_H
